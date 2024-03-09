ee286160b285f03daef5a0ba307155d9e70b798d
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-09 13:07:12 +1300

    Create schema gardern_club for proejct
    creating four tables and schema for garden club.

---
+++ app/garden_club.sql
@@ -0,0 +1,51 @@
+--CREATE SCHEMA IF NOT EXISTS garden_club;
+CREATE TABLE IF NOT EXISTS member (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    title VARCHAR(100),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    position VARCHAR(100),
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    address VARCHAR(100),
+    date_of_birth DATE,
+);
+
+CREATE TABLE IF NOT EXISTS instructor (
+    instructor_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    title VARCHAR(100),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    position VARCHAR(100),
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+    address VARCHAR(100),
+    instructor_profile TEXT,
+    instructor_image LONGBLOB,
+);
+
+CREATE TABLE IF NOT EXISTS manager (
+    manager_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    title VARCHAR(100),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    position VARCHAR(100),
+    phone_number VARCHAR(20),
+    email VARCHAR(100),
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100),
+    password VARCHAR(100),
+    role ENUM('Member', 'Instructor', 'Manager'),
+    related_manager_id INT,
+    related_instructor_id INT,
+    related_member_id INT,
+    FOREIGN KEY (related_manager_id) REFERENCES manager(manager_id) ON DELETE CASCADE,
+    FOREIGN KEY (related_instructor_id) REFERENCES instructor(instructor_id) ON DELETE CASCADE,
+    FOREIGN KEY (related_member_id) REFERENCES member(member_id) ON DELETE CASCADE
+);

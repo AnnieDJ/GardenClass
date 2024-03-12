fb580584762d703da19d2ad1e6f28d2ff126d100
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-13 12:52:55 +1300

    Update for instructor, manager and member framework
    Update for instructor, manager and member framework

--- app/home_views.py
+++ app/home_views.py
@@ -2,5 +2,13 @@ from app import app
 from flask import Flask, render_template
 
 @app.route('/')
+def home():
+    return render_template('base.html')
+@app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    return "Log Out"

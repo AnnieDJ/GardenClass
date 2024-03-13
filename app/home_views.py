97a0e44e4b61fbf1ae9b28a079193f8bd537bd54
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-13 14:01:24 +1300

    update home page
    update home page

--- app/home_views.py
+++ app/home_views.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 @app.route('/login')
 def login():
     return render_template('login.html')

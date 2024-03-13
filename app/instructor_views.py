69d8cd41c0de1e37586a6725963b4d49d37edeac
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-13 16:40:56 +1300

    Update manager and instructor
    Update manager and instructor

--- app/instructor_views.py
+++ app/instructor_views.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route("/instructor/dashboard")
 def instructor_dashboard():
-    return "instructor Dashboard"
+    return render_template("/instructor/instructordashboard.html")
 
 @app.route("/instructor/profile")
 def instructor_profile():

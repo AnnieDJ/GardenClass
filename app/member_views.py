24e674476e538e3360e695887d4f2a75809e0624
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-13 15:13:45 +1300

    update home page
    update home page

--- app/member_views.py
+++ app/member_views.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @app.route("/member/dashboard")
 def member_dashboard():
-    return render_template('member/smemberdashboard.html')
+    return render_template('/member/memberdashboard.html')
 
 @app.route("/member/profile")
 def member_profile():

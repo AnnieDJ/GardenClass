986965b430228a99efc7635e77a428917419eff5
Neal Nie <neal.nie@lincolnuni.co.nz>
2024-03-22 14:59:43 +1300

    Update role in instructor profile
    Update role in instructor profile

--- app/garden_club.sql
+++ app/garden_club.sql
@@ -158,10 +158,12 @@ CREATE TABLE IF NOT EXISTS payments (
 -- Create News table
 CREATE TABLE IF NOT EXISTS news (
     news_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
     title VARCHAR(255),
     content TEXT,
     date_published DATE,
     author_id INT,
+    FOREIGN KEY (user_id) REFERENCES member(member_id) ON DELETE CASCADE,
     FOREIGN KEY (author_id) REFERENCES manager(manager_id) ON DELETE CASCADE
 );
 

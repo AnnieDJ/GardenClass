48a3673aadeeda91c16ee703f63f9fac820c0c93
AnnieDJ <146434944+AnnieDJ@users.noreply.github.com>
2024-04-13 20:51:43 +1200

    Add manager add workshop picture
    Add manager add workshop picture

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 app.secret_key = 'the first secret key for schwifty'
 
+app.config['UPLOAD_FOLDER'] = 'app/static/workshops_images'  # Define where to store the uploaded files
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}  # Allowed upload file extensions
+
+
 from app import member_views
 from app import manager_views
 from app import instructor_views
